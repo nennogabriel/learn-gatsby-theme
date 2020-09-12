c3131dc778a1c113db2e2ad3cb9fba4e3567a86e
Pedro Moreno <nennogabriel@gmail.com>
2020-09-11 21:11:21 -0300

    07 - Detail Page

--- gatsby-theme-events/src/templates/event.js
+++ gatsby-theme-events/src/templates/event.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { graphql } from "gatsby"
 
-const EventTemplate = () => <p>TODO build the event page</p>
+import Layout from '../components/Layout';
+import Event from '../components/Event';
+
+
+export const query = graphql`
+  query ($eventID: String!) {
+    event(id: { eq: $eventID }) {
+      name
+      url
+      startDate(formatString: "MMMM D, YYYY")
+      location
+      slug
+    }
+  }
+`;
+
+const EventTemplate = ({ data: { event } }) => (
+  <Layout>
+    <Event {...event} />
+  </Layout>
+
+);
 
 export default EventTemplate
